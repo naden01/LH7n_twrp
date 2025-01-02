403f938085baa4a3c237879e06e6d73a7283eb4a
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-01-02 01:45:01 -0800

    shrp flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,3 +166,45 @@ ALLOW_MISSING_DEPENDENCIES := true
 
 # maintainer
 TW_DEVICE_VERSION := LH7n-Nazephyrus
+
+#SHRP Prop
+
+# NOTE - Dont use '-' or blank spaces in flag values , otherwise it will create build errors or other bugs in recovery (Excluding SHRP_PATH,SHRP_REC). 
+# Path of your SHRP Tree
+SHRP_PATH := device/tecno/LH7n
+
+# Maintainer name *
+SHRP_MAINTAINER := Nazephyrus-love-gardevoir
+
+# Device codename *
+SHRP_DEVICE_CODE := LH7n
+
+# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
+SHRP_REC_TYPE := Treble
+
+# Recovery Type (It can be A/B or A_only) [Only for About Section] *
+SHRP_DEVICE_TYPE := A/B
+# SHRP Padding Flag (Only for rounded corner devices.) [Optional]
+# You have to change these values according to your device's roundness.
+SHRP_STATUSBAR_RIGHT_PADDING := 40
+SHRP_STATUSBAR_LEFT_PADDING := 40
+
+# For Notch devices [Optional]
+SHRP_NOTCH := true
+
+# put this 0 if device has no EDL mode *
+SHRP_EDL_MODE := 0
+
+# Put your device's paths from fstab *
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
+
+# Use this flag only if your device is A/B *
+SHRP_AB := true
+
+# Check your device's recovery path, dont use blindly
+SHRP_REC := no_recovery_partition 
+
+# Put 0 to disable flashlight
+SHRP_FLASH := 0
