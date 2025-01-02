e2768e6f0831d8963b1079332ee2b0391793de9d
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-01-01 17:19:35 -0800

    bringup twrp
    LH7n

---
+++ README.md
@@ -0,0 +1,1 @@
+Device config for Tecno Pova 5 (LH7n)
---
+++ twrp_LH7n.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Inherit from LH7n device
+$(call inherit-product, device/tecno/LH7n/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Product Specifics
+PRODUCT_NAME := twrp_LH7n
+PRODUCT_DEVICE := LH7n
+PRODUCT_BRAND := Tecno
+PRODUCT_MODEL := Tecno LH7n
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion
